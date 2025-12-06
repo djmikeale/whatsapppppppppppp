323242ab18bc905712783c8e2e0a48b13e374d76
Mikael Thorup <mhthorup@gmail.com>
2025-12-06 08:48:23 +0100

    initialize project and add simple analysis

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lets you analyze your whatsapp messages
+
+```bash
+uv run --with jupyter jupyter lab
+```
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from whatsapppppppppppp!")
+
+
+if __name__ == "__main__":
+    main()
